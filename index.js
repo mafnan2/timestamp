f4b2217869d70bab9af97d89a5e06a133fd12881
mafnan2 <afnan.chkhan@gmail.com>
2025-05-07 14:41:06 +0500

    intiall commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,8 +20,25 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let dateString = req.params.date
+  let date;
+  if (!dateString) {
+    date = new Date()
+  } else {
+    if (!isNaN(dateString)) {
+      date = new Date(Number(dateString))
+    } else {
+      date = new Date(dateString)
+    }
+  }
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+  res.json({
+    unix: date.getTime(),          // Number (milliseconds)
+    utc: date.toUTCString()        // String (UTC format)
+  });
 });
 
 
